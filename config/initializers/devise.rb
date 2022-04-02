4c4b39bc1f4f443d8465ee5b07194c5cf44ff49c
Nikola <woleet@outlook.com>
2022-04-03 01:06:53 +0200

    Regenerated credentials and moved to credentials.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google_oauth2, :id), Rails.application.credentials.dig(:google_oauth2, :secret)
+  config.omniauth :github, Rails.application.credentials.dig(:github, :id), Rails.application.credentials.dig(:github, :secret), scope: 'user'
+  config.omniauth :twitter, Rails.application.credentials.dig(:twitter, :id), Rails.application.credentials.dig(:twitter, :secret)
+  config.omniauth :facebook, Rails.application.credentials.dig(:facebook, :id), Rails.application.credentials.dig(:facebook, :secret)
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
