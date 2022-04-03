580eee5424a2b9fdd176ef0b0dd4c93b28019175
Nikola <woleet@outlook.com>
2022-04-03 23:32:55 +0200

    Styling user views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def boolean_label(value)
+    if value
+      content_tag(:span, "Yes", class: "badge badge-danger")
+    else
+      content_tag(:span, "No", class: "badge badge-success")
+    end
+  end
 end
