bd1395b290fabcc77335f37f401a70a3108ed520
Nikola <woleet@outlook.com>
2022-04-03 23:01:59 +0200

    toastr for messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "bootstrap/dist/js/bootstrap";
-import "bootstrap/dist/css/bootstrap";
-import "@fortawesome/fontawesome-free/css/all";
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap"
+import "@fortawesome/fontawesome-free/css/all"
 
-require("stylesheets/application.scss");
+require("stylesheets/application.scss")
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+global.toastr = require("toastr")
+
+import "../stylesheets/application.scss"
